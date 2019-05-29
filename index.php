124b2558a73419a0197cfb543e1eded3d2d58284
Massau <joyce.santos202@etec.sp.gov.br>
2019-05-29 20:33:54 -0300

    Finaliza index do projeto

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ include("cabecalho.php");
 		</div>
 	</header>
 
-	<section id="justificativa" class="pt-0">
+	<section id="justificativa" class="py-0">
 
 	<div class="bd-example">
   <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
@@ -24,22 +24,20 @@ include("cabecalho.php");
       <div class="carousel-item active">
         <img src="assets/img/missao.jpg" class="d-block w-100" alt="..." style="height:85vh">
         <div class="carousel-caption d-none d-md-block">
-          <h5 class="intro-lead-in shadow">First slide label</h5>
-          <p class="intro-lead-in shadow">Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <!--h1 class="intro-lead-in shadow">Missão</h1>-->
+          <h3 class="intro-heading shadow">Nossa <b class="text-success">Missão</b> é beneficiar o maior número de pessoas, oferecendo um impacto real</h3>
         </div>
       </div>
       <div class="carousel-item">
-        <img src="assets/img/visao.jpg" class="d-block w-100" alt="...">
+        <img src="assets/img/visao.jpg" class="d-block w-100" alt="..." style="height:85vh">
         <div class="carousel-caption d-none d-md-block">
-          <h5>Second slide label</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+				<h5 class="intro-lead-in shadow">Temos por <b class="text-success">Missão</b>, sobretudo, auxiliar o produtor rural em suas dificuldades cotidianas quanto à venda de seus produtos</h5>
         </div>
       </div>
       <div class="carousel-item">
-        <img src="assets/img/valores.jpg" class="d-block w-100" alt="...">
+        <img src="assets/img/valores.jpg" class="d-block w-100" alt="..." style="height:85vh">
         <div class="carousel-caption d-none d-md-block">
-          <h5>Third slide label</h5>
-          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          <h5 class="intro-lead-in shadow">Nosso <b class="text-success">Valor</b> diz respeito à contribuir na produção e comercialização de alimentos, agregando na vida das pessoas em todos os estágios da produção</h5>
         </div>
       </div>
     </div>
@@ -53,74 +51,72 @@ include("cabecalho.php");
     </a>
   </div>
 </div>
-	<!--
-		<div class="container">
-			<div class="row">
-				<div class="row text-center">
-					<div class="col-lg-12 ">
-						<h2 class="section-heading">Justificativa</h2>
-						<h3 class="section-subheading text-muted">Das dificuldades do produtor rural</h3>
-					</div>
-				</div>
+	
+	</section>
 
-				<div class="row text-center">
-					<div class="col-lg-12">
+	<section style="background-image: url(assets/img/bgTrigo.png)">
+		<div class="container pt-0">
+			<div class="intro-text">
+			
+				<div class="row">
+					<div class="col-lg-8 text-center">
+						<h2 class="section-heading text-center">Justificativas</h2>
+						<h3 class="section-subheading text-muted">Das dificuldades do produtor rural</h3>
+						
+
+							<div class="card center" style="width: 40rem;">
+								<img class="card-img-top" src="assets/img/icone2.jpg" style="height: 20rem;">
+								<div class="card-body">
+									<p class="card-text justify">
+									<p>Micro e pequenos produtores rurais veem sua safra desvalorizada por intermediários entre si e as grandes distribuidoras que se conectam ao consumidor final.</p>
+									<p>Temos por objetivo intemediar essa relação, reduzindo encargos ao trabalhador que compõe a base da cadeia produtiva.</p>
+									</p>
+								</div>
+							</div>
+					</div>
+				
+
+					<div class="col-lg-4 text-center">
+					<h2 class="section-heading">Uma iniciativa</h2>
+					<h3 class="section-subheading text-muted">Idealizadores</h3>
+
 						<ul class="timeline">
 							<li>
 								<div class="timeline-image">
-									<img class="rounded-circle img-fluid" src="assets/img/icone3.jpg">
+									<img class="rounded-circle img-fluid" src="assets/img/usuario.png">
 								</div>
 							</li>
 						</ul><br>
-						<p>Micro e pequenos produtores rurais veem sua safra desvalorizada por intermediários entre si e as grandes distribuidoras que se conectam ao consumidor final.</p>
-						<p>Temos por objetivo intemediar essa relação, reduzindo encargos ao trabalhador que compõe a base da cadeia produtiva.</p>
-					</div>
-				</div>		
-			</div>
-		</div>-->
-	</section>
+						<h3 class="text-light">Nicolas</h3>
+	
+					<!--</div>-->
 
-	<section id="topicos">
-		<div class="container">
-			<div class="row">
-				<div class="row">
-					<div class="col-lg-12 text-center">
-						<h2 class="section-heading">Missão  |  Visão  |  Valores</h2>
-						<h3 class="section-subheading text-muted">Das dificuldades do produtor rural</h3>
-					</div>
-				</div>
-
-				<div class="row text-center">
-					<div class="col-lg-12">
+					<!--<div class="col-lg-4">-->
 						<ul class="timeline">
 							<li>
 								<div class="timeline-image">
-									<img class="rounded-circle img-fluid" src="assets/img/icone2.jpg">
+									<img class="rounded-circle img-fluid" src="assets/img/usuario.png">
 								</div>
 							</li>
-						</ul>
-						<div class="row">
-							<div class="col-lg-4">
-								<h3>Nossa Missão</h3>
-								<p class="bgBranco">Viabilizar soluções ao pequeno produtor, desenvolvimer e inovar para a sustentabilidade da agricultura em benefício da sociedade brasileira.</p>
-							</div>
-							<div class="col-lg-4">
-								<h3>Nossa Visão</h3>
-								<p class="bgBranco">Contribuir com o pequeno produtor rural através de oferta de informações, conhecimentos e tecnologias, auxiliando para a inovação, sustentabilidade da agricultura e segurança alimentar.</p>
-							</div>
-							<div class="col-lg-4">
-								<h3>Nossos valores</h3>
-								<p class="bgBranco">Ter equidade e Responsabilidade Socioambiental como princípios fundamentais.</p>
-							</div>
-						</div>
-						
-					</div>
-				</div>		
+						</ul><br>
+						<h3 class="text-light">Joyce</h3>
+					
+					<!--</div>-->
+
+
+
+				</div>
+
+				
+
 			</div>
 		</div>
 	</section>
 
-	<footer class="bg-success">
+
+
+	<footer>
+		<img src="assets/img/etec.png" style="height: 50px;" class="pr-2">
 		Projeto desenvolvido para as disciplinas do curso de Informática para internet - ETEC prof Antônio Devisate
 	</footer>
 	
